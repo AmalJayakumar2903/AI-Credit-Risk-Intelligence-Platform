43ad4a700c07981844949ae48575f7c8a8b1657a
Amal Jayakumar <amal003@gmail.com>
2026-06-20 12:28:49 +0530

    Add FastAPI end points for baseline analytics

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from fastapi import UploadFile
+from fastapi import File
+
+import shutil
+
+from backend.ingestion.excel_loader import load_file
+from backend.pipeline import run_analysis
 
 app = FastAPI()
 
@@ -9,4 +16,39 @@ def home():
     return {
         "status": "running",
         "application": "CredRiskAnalyzer"
+    }
+
+
+@app.post("/analyze")
+async def analyze_file(
+    file: UploadFile = File(...)
+):
+
+    temp_path = (
+        f"data/raw/{file.filename}"
+    )
+
+    with open(
+        temp_path,
+        "wb"
+    ) as buffer:
+
+        shutil.copyfileobj(
+            file.file,
+            buffer
+        )
+
+    df = load_file(
+        temp_path,
+        nrows=2000
+    )
+
+    result = run_analysis(df)
+
+    return {
+    "portfolio": result["portfolio"],
+    "risk": result["risk"],
+    "segmentation": result["segmentation"],
+    "recommendations": result["recommendations"],
+    "insight": result["insight"]
     }
